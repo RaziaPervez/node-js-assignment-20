627cbb75f694b750a866a5ef4a51e6e529dcce4e
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 10:26:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//project=20
+//Think of something you could store in a array. For example, you could make a list of 
+//mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+//making array of mountains name
+var mountainNames = ["k2", "mount everest", "himalayan", "karakoram"];
+//printing the message list
+console.log("list of mountain name");
+//print the array in the form of list
+mountainNames.forEach(function (mountain) { return console.log(mountain); });
